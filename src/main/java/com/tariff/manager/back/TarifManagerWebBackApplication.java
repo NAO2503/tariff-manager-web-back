11d6420079583c3c2c5d7c40acf8830411cd0e43
Napoleon Avila Ochoa <e_navila@mercadona.com>
2024-11-05 14:55:11 +0100

    feat: :boom: first commit

---
+++ src/main/java/com/tariff/manager/back/TarifManagerWebBackApplication.java
@@ -0,0 +1,13 @@
+package com.tariff.manager.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarifManagerWebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarifManagerWebBackApplication.class, args);
+	}
+
+}
