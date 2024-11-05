3d189bd5a24037cc352728cb5ea01151700ec89e
Napoleon Avila Ochoa <e_navila@mercadona.com>
2024-11-05 22:28:36 +0100

    test: :white_check_mark: Include tests in boot module

---
+++ boot/src/test/java/com/tariff/manager/back/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.tariff.manager.back;
+
+import org.junit.jupiter.api.Test;// Test class added ONLY to cover main() invocation not covered by application tests.
+
+public class ApplicationTest {
+
+    @Test
+    void main() {
+        Application.main(new String[]{});
+    }
+}
